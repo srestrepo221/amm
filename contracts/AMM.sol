b486a71130c2c4623c6fe9fa904539d9fe760396
santiago restrepo <srest031@fiu.edu>
2022-11-24 00:27:32 -0500

    Add liquidityP1

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -13,10 +13,54 @@ import "./Token.sol";
 contract AMM {
 
 	Token public token1;
-	Token public token2; 
+	Token public token2;
+
+	uint256 public token1Balance;
+	uint256 public token2Balance;
+	uint256 public K; // Constant used for our pricing formula X*Y=K
+
+	uint256 public totalShares;
+	mapping(address => uint256) public shares;
+	uint256 constant PRECISION = 10**18;
 
 	constructor(Token _token1, Token _token2){
 		token1 = _token1;
 		token2 = _token2;
 	}
+
+	function addLiquidity(uint256 _token1Amount, uint256 _token2Amount)  external {
+		// Deposit Tokens
+		require(
+			token1.transferFrom(msg.sender,  address(this), _token1Amount),
+			"failed to transfer token1"
+		);
+
+		require(
+			token2.transferFrom(msg.sender,  address(this), _token2Amount),
+			"message"
+		);
+
+		//Issue Shares
+		uint256 share;
+
+		// If first time adding liquidity, make share 100;
+		if(totalShares == 0) {
+			share = 100 * PRECISION;
+		} else {
+
+		}
+
+		// Manage Pool
+		token1Balance += _token1Amount;
+		token2Balance += _token2Amount;
+		K = token1Balance * token2Balance;
+
+		// Updates shares
+		totalShares += share;
+		shares[msg.sender]+= share;
+	}
+
 }
+
+
+
--- test/AMM.js
+++ test/AMM.js
@@ -8,16 +8,35 @@ const tokens = (n) => {
 const ether = tokens
 
 describe('AMM', () => {
-  let accounts, deployer, token1, token2, amm
+  let accounts,
+      deployer,
+      liquidityProvider
+
+  let token1,
+      token2,
+      amm
 
   beforeEach(async () => {
+    // Setup  accounts
+
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    liquidityProvider  = accounts[1]
 
+
+    // Deploy Token
     const Token = await ethers.getContractFactory('Token')
     token1 = await Token.deploy('Dapp University', 'DAPP', '1000000') // 1 Million Tokens
     token2 = await Token.deploy('Dapp University', 'USD', '1000000') // 1 Million Tokens
   
+    // Send tokens to liquidity provider
+    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    //Deploy AMM
     const AMM = await ethers.getContractFactory('AMM')
     amm = await AMM.deploy(token1.address, token2.address)
   })
@@ -35,7 +54,52 @@ describe('AMM', () => {
     it('tracks token2 address', async () => {
       expect(await amm.token2()).to.equal(token2.address)
     })
+
   })
+
+    describe('Swapping tokens', async () => {
+      let amount, transaction, result
+
+      it('facilitates swap', async () => {
+        //Deployer approves 100k tokens
+        amount = tokens(100000)
+        transaction = await token1.connect(deployer).approve(amm.address, amount)
+        await transaction.wait()
+
+        transaction = await token2.connect(deployer).approve(amm.address, amount)
+        await transaction.wait()
+
+        // Deployer adds liquidity
+        transaction = await  amm.connect(deployer).addLiquidity(amount, amount)
+        await transaction.wait()
+
+        // Check AMM receives tokens
+        expect(await token1.balanceOf(amm.address)).to.equal(amount)
+        expect(await token2.balanceOf(amm.address)).to.equal(amount)
+
+        expect(await amm.token1Balance()).to.equal(amount)
+        expect(await amm.token2Balance()).to.equal(amount)
+
+        // Check deployer has 100 shares
+        expect(await amm.shares(deployer.address)).to.equal(tokens(100))
+
+        // Check pool has 100 shares
+        expect(await amm.totalShares()).to.equal(tokens(100))
+
+      })
+
+
+    })
+
 })
+
+
+
+
+
+
+
+
+
 
   
