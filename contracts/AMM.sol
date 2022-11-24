b12465a2f048b8e8b6ab70352bcad970ead6528e
santiago restrepo <srest031@fiu.edu>
2022-11-23 23:29:46 -0500

    Create AMM

---
+++ contracts/AMM.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./Token.sol";
+
+
+//[] Manage Pool
+//[] Manage Deposits
+//[] Facilitate swaps (i.e. Trades)
+//[] Manage Withdraws
+
+contract AMM {
+
+	Token public token1;
+	Token public token2; 
+
+	constructor(Token _token1, Token _token2){
+		token1 = _token1;
+		token2 = _token2;
+	}
+}
---
+++ test/AMM.js
@@ -0,0 +1,41 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+describe('AMM', () => {
+  let accounts, deployer, token1, token2, amm
+
+  beforeEach(async () => {
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+
+    const Token = await ethers.getContractFactory('Token')
+    token1 = await Token.deploy('Dapp University', 'DAPP', '1000000') // 1 Million Tokens
+    token2 = await Token.deploy('Dapp University', 'USD', '1000000') // 1 Million Tokens
+  
+    const AMM = await ethers.getContractFactory('AMM')
+    amm = await AMM.deploy(token1.address, token2.address)
+  })
+
+  describe('Deployment', () => {
+
+    it('has an address', async () => {
+      expect(amm.address).to.not.equal(0x0)
+    })
+
+    it('tracks token1 address', async () => {
+      expect(await amm.token1()).to.equal(token1.address)
+    })
+
+    it('tracks token2 address', async () => {
+      expect(await amm.token2()).to.equal(token2.address)
+    })
+  })
+})
+
+  
