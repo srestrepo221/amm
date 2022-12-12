24d21087386b4abfc41ed58b2d887437e799d47e
santiago restrepo <srest031@fiu.edu>
2022-12-12 18:16:19 -0500

    Finished charts page

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -3,30 +3,15 @@ import Table from 'react-bootstrap/Table';
 import Chart from 'react-apexcharts';
 import { ethers } from 'ethers';
 
+import { options, series } from './Charts.config';
+import { chartSelector } from '../store/selectors';
 import { useEffect } from 'react'
 
-//import Loading from './Loading';
+import Loading from './Loading';
 
 import { 
 	loadAllSwaps
 } from '../store/interactions'
-
-const STATE = {
-	options: {
-	chart:  {
-		id: "basic-bar"
-	},
-	xaxis: {
-		categories: [1991,1992,1993,1994,1995,1996]
-	}
-},
-	series : [
-		{
-			name: "series-1",
-			data:[30,40,45,50,55,60]
-		}
-	]
-};
 
 const Charts = () => {
 	const provider = useSelector(state => state.provider.connection)
@@ -35,7 +20,8 @@ const Charts = () => {
 	const symbols = useSelector(state => state.tokens.symbols)
 
 	const amm = useSelector(state => state.amm.contract)
-	const swaps = useSelector(state => state.amm.swaps)
+
+	const chart = useSelector(chartSelector)
 
 	const dispatch = useDispatch()
 
@@ -47,58 +33,66 @@ const Charts = () => {
 
 	return (
 		<div>
-			<Chart
-				options={STATE.options}
-				series={STATE.series}
-				type="bar"
-				width="500"
-			/>
+			{provider && amm ? (
+				<div>
 
-			<hr />
+					<Chart
+					type="line"
+					options={options}
+					series={chart ? chart.series : series}
+					width="100%"
+					height="100%"
+				/>
 
-			<Table striped bordered={1} hover>
-			<thead>
-				<tr>
-					<th>Transaction Hash</th>
-					<th>Token Give</th>
-					<th>Amount Give</th>
-					<th>Token Get</th>
-					<th>Token Give</th>
-					<th>User</th>
-					<th>Time</th>
-				</tr>
-			</thead>
-			<tbody>
-				{swaps && swaps.map((swap, index) => (
-					<tr key={index}>
-					<td>{swap.hash.slice(0, 5) + '...' + swap.hash.slice(61, 66)}</td>
-					<td>{swap.args.tokenGive === tokens[0].address ? symbols[0] : symbols[1]}</td>
-					<td>{ethers.utils.formatUnits(swap.args.tokenGiveAmount.toString(), 'ether')}</td>
-					<td>{swap.args.tokenGet === tokens[0].address ? symbols[0] : symbols[1]}</td>
-					<td>{ethers.utils.formatUnits(swap.args.tokenGetAmount.toString(), 'ether')}</td>
-					<td>{swap.args.user.slice(0, 5) + '...' + swap.args.user.slice(38, 42)}</td>
-					<td>{
+				<hr />
 
-						new Date(Number(swap.args.timestamp.toString() + '000'))
-						.toLocaleDateString(
-							undefined, 
-							{
-							year: 'numeric',
-							month: 'long',
-							day: 'numeric',
-							hour: 'numeric',
-							minute: 'numeric',
-							second: 'numeric'
-							}
-						)
-					}</td>
-				</tr>
-				))}
-				
-			</tbody>
-		</Table>
+				<Table striped bordered={1} hover>
+				<thead>
+					<tr>
+						<th>Transaction Hash</th>
+						<th>Token Give</th>
+						<th>Amount Give</th>
+						<th>Token Get</th>
+						<th>Token Give</th>
+						<th>User</th>
+						<th>Time</th>
+					</tr>
+				</thead>
+				<tbody>
+					{chart.swaps && chart.swaps.map((swap, index) => (
+						<tr key={index}>
+						<td>{swap.hash.slice(0, 5) + '...' + swap.hash.slice(61, 66)}</td>
+						<td>{swap.args.tokenGive === tokens[0].address ? symbols[0] : symbols[1]}</td>
+						<td>{ethers.utils.formatUnits(swap.args.tokenGiveAmount.toString(), 'ether')}</td>
+						<td>{swap.args.tokenGet === tokens[0].address ? symbols[0] : symbols[1]}</td>
+						<td>{ethers.utils.formatUnits(swap.args.tokenGetAmount.toString(), 'ether')}</td>
+						<td>{swap.args.user.slice(0, 5) + '...' + swap.args.user.slice(38, 42)}</td>
+						<td>{
+
+							new Date(Number(swap.args.timestamp.toString() + '000'))
+							.toLocaleDateString(
+								undefined, 
+								{
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+								hour: 'numeric',
+								minute: 'numeric',
+								second: 'numeric'
+								}
+							)
+						}</td>
+					</tr>
+					))}
+				</tbody>
+			</Table>
 		</div>
-		
+
+		) : (
+		<Loading/>
+		)}
+
+		</div>
 	);
 }
 
